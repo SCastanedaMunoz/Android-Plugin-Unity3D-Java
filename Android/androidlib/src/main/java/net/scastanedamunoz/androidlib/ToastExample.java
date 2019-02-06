2abaaafc05d4bba28867f78dc83812e34280b36f
Santiago Castaneda Munoz <santiagocm98@hotmail.com>
2019-02-05 20:50:42 -0800

    Adding Toast Notification Plugin Example

---
+++ Android/androidlib/src/main/java/net/scastanedamunoz/androidlib/ToastExample.java
@@ -0,0 +1,29 @@
+package net.scastanedamunoz.androidlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastExample
+{
+    private Context context;
+    private static ToastExample instance;
+
+    public ToastExample() {
+        this.instance = this;
+    }
+
+    public static ToastExample instance() {
+        if(instance == null) {
+            instance = new ToastExample();
+        }
+        return instance;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void showMessage(String message) {
+        Toast.makeText(this.context, message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ Unity/AndroidPluginUnity/Assets/Scripts/UnityToastExample.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class UnityToastExample : MonoBehaviour
+{
+    private AndroidJavaObject toastExample = null;
+    private AndroidJavaObject activityContext = null;
+
+    void Start()
+    {
+        if (toastExample == null)
+        {
+            using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+            }
+
+            using (AndroidJavaClass pluginClass = new AndroidJavaClass("net.scastanedamunoz.androidlib.ToastExample"))
+            {
+                if (pluginClass != null)
+                {
+                    toastExample = pluginClass.CallStatic<AndroidJavaObject>("instance");
+                    toastExample.Call("setContext", activityContext);
+                    activityContext.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+                        toastExample.Call("showMessage", "This is a Toast message");
+                    }));
+                }
+            }
+        }
+    }
+}
