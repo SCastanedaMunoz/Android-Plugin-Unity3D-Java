de9be2ee75399bddf99607b08d6e41bffdba11cf
Santiago Castaneda Munoz <santiagocm98@hotmail.com>
2019-02-07 23:29:57 -0500

    Finalizing Screenshot Saving to Device
    * Saving Screenshot to iOS Device using Native functionality.

--- Unity/AndroidPluginUnity/Assets/Plugins/iOS/MyPlugin.mm
+++ Unity/AndroidPluginUnity/Assets/Plugins/iOS/MyPlugin.mm
@@ -7,6 +7,9 @@ typedef void (*INT_CALLBACK)(int);
 {
     INT_CALLBACK alertCallBack;
     NSDate *creationDate;
+    
+    INT_CALLBACK shareCallBack;
+    UIPopoverController *popover;
 }
 @end
 
@@ -16,13 +19,13 @@ static MyPlugin *_sharedInstance;
 
 +(MyPlugin*) sharedInstance
 {
-	static dispatch_once_t onceToken;
-	dispatch_once(&onceToken, ^{
-		NSLog(@"Creating MyPlugin shared instance");
-		_sharedInstance = [[MyPlugin alloc] init];
-	});
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"Creating MyPlugin shared instance");
+        _sharedInstance = [[MyPlugin alloc] init];
+    });
     
-	return _sharedInstance;
+    return _sharedInstance;
 }
 
 +(NSString*)createNSString:(const char*) string
@@ -72,21 +75,69 @@ static MyPlugin *_sharedInstance;
 
 -(id)init
 {
-	self = [super init];
-	if(self)
-		[self initHelper];
-	return self;
+    self = [super init];
+    if(self)
+        [self initHelper];
+    return self;
 }
 
 -(void)initHelper
 {
-	NSLog(@"initHelper called");
+    NSLog(@"initHelper called");
     creationDate = [NSDate date];
 }
 
 -(double)getElapsedTime
 {
     return[[NSDate date] timeIntervalSinceDate:creationDate];
+}
+
+
+-(void)shareScreenImage:(const unsigned char*)imagePNG_in length:(long)length caption:(const char*) caption_in callback:(INT_CALLBACK)callback
+{
+    NSMutableArray *shareableItems = [NSMutableArray arrayWithCapacity:2];
+    NSString *caption;
+    UIImage *image;
+    if(caption_in != nil)
+    {
+        caption = [MyPlugin createNSString:caption_in];
+        [shareableItems addObject:caption];
+    }
+    
+    if(imagePNG_in != nil)
+    {
+        NSData *pngData = [NSData dataWithBytes:imagePNG_in length:length];
+        image = [UIImage imageWithData:pngData];
+        [shareableItems addObject:image];
+        pngData = nil;
+    }
+    
+    shareCallBack = callback;
+    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:shareableItems applicationActivities:nil];
+    
+    activityViewController.completionWithItemsHandler = ^(NSString *activityType, BOOL completed, NSArray *returnedItems, NSError *activityError)
+    {
+        NSLog(@"Activity %@ completed: %d", activityType, completed);
+        if(activityError != nil)
+            NSLog(@"Error: %@", [activityError localizedDescription]);
+        if(shareCallBack != nil)
+            shareCallBack(completed);
+    };
+    
+    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+    {
+        [UnityGetGLViewController() presentViewController:activityViewController animated:YES completion:^{
+            NSLog(@"share presented");
+        }];
+    }
+    
+    else
+    {
+        popover = [[UIPopoverController alloc] initWithContentViewController:activityViewController];
+        UIView *mainView = UnityGetGLView();
+        popover.delegate = nil;
+        [popover presentPopoverFromRect:CGRectMake(mainView.frame.size.width/2, mainView.frame.size.height -10,0,0) inView:mainView permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
 }
 @end
 
@@ -102,4 +153,11 @@ extern "C"
     {
         [[MyPlugin sharedInstance] createAlertDialog:strings stringCount:stringCount callback:callback];
     }
+    
+    void IOSshareScreenImage(const unsigned char* imagePNG, long imageLen, const char* caption, INT_CALLBACK callback)
+    {
+        [[MyPlugin sharedInstance] shareScreenImage:imagePNG length:imageLen caption:caption callback:callback];
+    }
+    
 }
+
