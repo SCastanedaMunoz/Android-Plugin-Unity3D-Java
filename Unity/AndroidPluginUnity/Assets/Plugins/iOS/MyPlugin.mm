22782927e4848f0735c985ac52c70c00c3b83008
Santiago Castaneda Munoz <santiagocm98@hotmail.com>
2019-02-07 16:30:52 -0800

    Adding Structure for iOS Plugins with Unity
    * Adding Object C basic Plugin.

---
+++ Unity/AndroidPluginUnity/Assets/Plugins/iOS/MyPlugin.mm
@@ -0,0 +1,36 @@
+﻿#import <Foundation/Foundation.h>
+
+@inteface MyPlugin: NSObject
+{
+
+}
+@end
+
+@implementation MyPlugin
+
+static MyPlugin *_sharedInstance;
+
++(MyPlugin*) sharedInstance
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		NSLog(@"Creating MyPlugin shared instance");
+		_sharedInstance = [[MyPlugin alloc] init];
+	});
+
+	return _sharedInstance;
+}
+
+-(id)init
+{
+	self = [super init];
+	if(self)
+		[self initHelper];
+	return self;
+}
+
+-(void)initHelper
+{
+	NSLog(@"initHelper called");
+}
+@end
